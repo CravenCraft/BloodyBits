9bac1f7c581e6b6c9c9213d60485392a1e061ceb
CravenCraft <benjamin.r.craven@gmail.com>
2026-08-03 00:44:37 -0400

    Blood drip is mostly where I want it.

--- src/main/java/com/cravencraft/bloodybits/particle/drip/BloodDripParticle.java
+++ src/main/java/com/cravencraft/bloodybits/particle/drip/BloodDripParticle.java
@@ -3,18 +3,25 @@ package com.cravencraft.bloodybits.particle.drip;
 import com.cravencraft.bloodybits.BloodyBitsMod;
 import com.cravencraft.bloodybits.particle.BloodSprayParticleOptions;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Axis;
 import net.minecraft.client.Camera;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.*;
 import net.minecraft.core.Direction;
+import net.minecraft.util.Mth;
 import net.minecraft.world.phys.AABB;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.joml.Quaternionf;
 import org.joml.Vector3f;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BloodDripParticle extends TextureSheetParticle {
+    private static final float DEGREES_90 = Mth.PI / 2f;
     private final Direction direction;
     private float dripAmount;
 //    private final TextureAtlasSprite sprite;
@@ -45,13 +52,19 @@ public class BloodDripParticle extends TextureSheetParticle {
     public void render(@NotNull VertexConsumer buffer, @NotNull Camera camera, float partialTick) {
 
 //        if (this.direction ==  Direction.UP) {
-//            var yPos = Math.max(this.y + 0.5F, this.floor);
-//            if (this.yd != 0) {
-//                var shrinkAmount = (float) (this.thickness / Math.abs((this.ceiling - this.floor) / this.yd));
-//                this.thickness -= shrinkAmount;
+//            Quaternionf quaternionf = new Quaternionf();
+//            this.getFacingCameraMode().setRotation(quaternionf, camera, partialTick);
+//            if (this.roll != 0.0F) {
+//                quaternionf.rotateZ(Mth.lerp(partialTick, this.oRoll, this.roll));
 //            }
 //
-//            super.render(buffer, camera, partialTick);
+////            var yPos = Math.max(this.y + 0.5F, this.floor);
+////            if (this.yd != 0) {
+////                var shrinkAmount = (float) (this.thickness / Math.abs((this.ceiling - this.floor) / this.yd));
+////                this.thickness -= shrinkAmount;
+////            }
+////
+////            super.render(buffer, camera, partialTick);
 //        }
 
         var yPos = Math.max(this.y + 0.5F, this.floor);
@@ -67,12 +80,52 @@ public class BloodDripParticle extends TextureSheetParticle {
 //        float x = (float) this.x;
 //        float y = (float) this.y - dripAmount;
 //        float z = (float) this.z;
-        BloodyBitsMod.LOGGER.info("yd: {} thickness: {}", this.yd, this.thickness);
+//        BloodyBitsMod.LOGGER.info("yd: {} thickness: {}", this.yd, this.thickness);
 //        BloodyBitsMod.LOGGER.info("------- Rendering Blood Drip Particle -------");
-        this.renderVertex(buffer, camera, (float) this.x + this.thickness, (float) yPos, (float) z, this.sprite.getU1(), this.sprite.getV0(), partialTick);
-        this.renderVertex(buffer, camera, (float) this.x - this.thickness, (float) yPos, (float) z, this.sprite.getU0(), this.sprite.getV0(), partialTick);
-        this.renderVertex(buffer, camera, (float) this.x - this.thickness, (float) this.ceiling, (float) z, this.sprite.getU0(), this.sprite.getV1(), partialTick);
-        this.renderVertex(buffer, camera, (float) this.x + this.thickness, (float) this.ceiling, (float) z, this.sprite.getU1(), this.sprite.getV1(), partialTick);
+
+        Vector3f xVector1;
+        Vector3f xVector2;
+        Vector3f xVector3;
+        Vector3f xVector4;
+        // TODO: Simplify this in the future. Maybe find a way to use rotations?
+        if (this.direction == Direction.UP) {
+
+            // Render X-Axis blood drip.
+            xVector1 = new Vector3f((float) ((this.x + this.thickness) - camera.getPosition().x), (float) (yPos - camera.getPosition().y), (float) (this.z - camera.getPosition().z));
+            xVector2 = new Vector3f((float) ((this.x - this.thickness) - camera.getPosition().x), (float) (yPos - camera.getPosition().y), (float) (this.z - camera.getPosition().z));
+            xVector3 = new Vector3f((float) ((this.x - this.thickness) - camera.getPosition().x), (float) (this.ceiling - camera.getPosition().y), (float) (this.z - camera.getPosition().z));
+            xVector4 = new Vector3f((float) ((this.x + this.thickness) - camera.getPosition().x), (float) (this.ceiling - camera.getPosition().y), (float) (this.z - camera.getPosition().z));
+
+            this.renderVertex(buffer, camera, xVector1.x, xVector1.y, xVector1.z, this.sprite.getU1(), this.sprite.getV0(), partialTick);
+            this.renderVertex(buffer, camera, xVector2.x, xVector2.y, xVector2.z, this.sprite.getU0(), this.sprite.getV0(), partialTick);
+            this.renderVertex(buffer, camera, xVector3.x, xVector3.y, xVector3.z, this.sprite.getU0(), this.sprite.getV1(), partialTick);
+            this.renderVertex(buffer, camera, xVector4.x, xVector4.y, xVector4.z, this.sprite.getU1(), this.sprite.getV1(), partialTick);
+
+            this.renderVertex(buffer, camera, xVector4.x, xVector4.y, xVector4.z, this.sprite.getU1(), this.sprite.getV1(), partialTick);
+            this.renderVertex(buffer, camera, xVector3.x, xVector3.y, xVector3.z, this.sprite.getU0(), this.sprite.getV1(), partialTick);
+            this.renderVertex(buffer, camera, xVector2.x, xVector2.y, xVector2.z, this.sprite.getU0(), this.sprite.getV0(), partialTick);
+            this.renderVertex(buffer, camera, xVector1.x, xVector1.y, xVector1.z, this.sprite.getU1(), this.sprite.getV0(), partialTick);
+
+            // Render Z-Axis blood drip
+            var zVector1 = new Vector3f((float) (this.x - camera.getPosition().x), (float) (yPos - camera.getPosition().y), (float) ((this.z + this.thickness) - camera.getPosition().z));
+            var zVector2 = new Vector3f((float) (this.x - camera.getPosition().x), (float) (yPos - camera.getPosition().y), (float) ((this.z - this.thickness) - camera.getPosition().z));
+            var zVector3 = new Vector3f((float) (this.x - camera.getPosition().x), (float) (this.ceiling - camera.getPosition().y), (float) ((this.z - this.thickness) - camera.getPosition().z));
+            var zVector4 = new Vector3f((float) (this.x - camera.getPosition().x), (float) (this.ceiling - camera.getPosition().y), (float) ((this.z + this.thickness) - camera.getPosition().z));
+
+            this.renderVertex(buffer, camera, zVector1.x, zVector1.y, zVector1.z, this.sprite.getU1(), this.sprite.getV0(), partialTick);
+            this.renderVertex(buffer, camera, zVector2.x, zVector2.y, zVector2.z, this.sprite.getU0(), this.sprite.getV0(), partialTick);
+            this.renderVertex(buffer, camera, zVector3.x, zVector3.y, zVector3.z, this.sprite.getU0(), this.sprite.getV1(), partialTick);
+            this.renderVertex(buffer, camera, zVector4.x, zVector4.y, zVector4.z, this.sprite.getU1(), this.sprite.getV1(), partialTick);
+
+
+            this.renderVertex(buffer, camera, zVector4.x, zVector4.y, zVector4.z, this.sprite.getU1(), this.sprite.getV1(), partialTick);
+            this.renderVertex(buffer, camera, zVector3.x, zVector3.y, zVector3.z, this.sprite.getU0(), this.sprite.getV1(), partialTick);
+            this.renderVertex(buffer, camera, zVector2.x, zVector2.y, zVector2.z, this.sprite.getU0(), this.sprite.getV0(), partialTick);
+            this.renderVertex(buffer, camera, zVector1.x, zVector1.y, zVector1.z, this.sprite.getU1(), this.sprite.getV0(), partialTick);
+        }
+        else {
+
+        }
     }
 
     private void renderVertex(VertexConsumer buffer, Camera camera, float x, float y, float z, float u, float v, float partialTick) {
@@ -80,27 +133,46 @@ public class BloodDripParticle extends TextureSheetParticle {
 //        BloodyBitsMod.LOGGER.info("camera facing position: {}", camera.getPosition());
 //        BloodyBitsMod.LOGGER.info("actual vertex pos: {}, {}, {}", (float) (x - camera.getPosition().x), (float) (y - camera.getPosition().y), (float) (z - camera.getPosition().z));
 //        BloodyBitsMod.LOGGER.info("bounding box: {}", this.getBoundingBox());
+        buffer
+                .addVertex(x, y, z)
+                .setColor(this.rCol, this.gCol, this.bCol, this.alpha)
+                .setUv(u, v)
+                .setLight(this.getLightColor(partialTick));
 
-        if (this.direction == Direction.UP) {
+    }
 
-            Vector3f front = new Vector3f(
-                    (float) (x - camera.getPosition().x),
-                    (float) (y - camera.getPosition().y),
-                    (float) (z - camera.getPosition().z));
+    private List<Vector3f> createBloodDripXVectors(float dripLength, Camera camera) {
+        var dripVectors = new ArrayList<Vector3f>(4);
 
-            buffer.addVertex(front)
-                    .setUv(u, v)
-                    .setColor(this.rCol, this.gCol, this.bCol, this.alpha)
-                    .setLight(this.getLightColor(partialTick));
-        }
-        else {
-            buffer
-                    .addVertex((float) (x - camera.getPosition().x), (float) (y - camera.getPosition().y), (float) (z - camera.getPosition().z))
-                    .setColor(this.rCol, this.gCol, this.bCol, this.alpha)
-                    .setUv(u, v)
-                    .setLight(this.getLightColor(partialTick));
-        }
+        var vector1 = new Vector3f(
+                (float) ((this.x + this.thickness) - camera.getPosition().x),
+                (float) (dripLength - camera.getPosition().y),
+                (float) (this.z - camera.getPosition().z));
+        var vector2 = new Vector3f(
+                (float) ((this.x - this.thickness) - camera.getPosition().x),
+                (float) (dripLength - camera.getPosition().y),
+                (float) (this.z - camera.getPosition().z));
+        var vector3 = new Vector3f(
+                (float) ((this.x - this.thickness) - camera.getPosition().x),
+                (float) (this.ceiling - camera.getPosition().y),
+                (float) (this.z - camera.getPosition().z));
+        var vector4 = new Vector3f(
+                (float) ((this.x + this.thickness) - camera.getPosition().x),
+                (float) (this.ceiling - camera.getPosition().y),
+                (float) (this.z - camera.getPosition().z));
 
+        dripVectors.add(vector1);
+        dripVectors.add(vector2);
+        dripVectors.add(vector3);
+        dripVectors.add(vector4);
+
+        return dripVectors;
+    }
+
+    private List<Vector3f> createBloodDripZVectors(float x, float dripLength, float z) {
+        var dripVectors = new ArrayList<Vector3f>(4);
+
+        return dripVectors;
     }
 
     @Override
